f1d494a159532a153d685d58aba14b73f888a02c
VladislavNahaba <nahabavladislav@gmail.com>
2023-07-28 11:56:59 +0300

    improve seo components

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -12,6 +12,15 @@ export function Seo({
     <Helmet titleTemplate={`%s | Coffee Shop`}>
       <meta name="description" content={description} />
       <meta property="og:type" content="website" />
+      <meta property="og:url" content="Coffeeshop.org" />
+      <meta property="og:title" content="Coffee Shop" />
+      <meta property="og:description" content="Coffee Shop for maniacs" />
+      <meta
+        property="og:image"
+        content="https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcS5iKBhe62Mbj3CavwfKcI3qEhN2U8KryfZnGyQXjb-h8A9ctiF-dDtHlDLptk-mK2s"
+      />
+      <meta property="og:image:alt" content="Coffee Shop for maniacs" />
+      <meta property="og:site_name" content="Coffee Shop" />
       {children}
     </Helmet>
   );
